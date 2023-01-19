a6712a6f0b1336ca2136b245fcb3364345771d91
oisouguilherme <gh.dourado77@gmail.com>
2023-01-19 17:40:33 -0300

    edit-summary

--- src/components/Perguntas/Pergunta/Pergunta.jsx
+++ src/components/Perguntas/Pergunta/Pergunta.jsx
@@ -5,9 +5,18 @@ function Pergunta(){
     return(
         <div>
             <div className='pergunta'>
-                <img src={Plus} alt="" />
-                <p>O perpétuo pode atrapalhar o resultado do lançamento?</p>
-
+                    <img src={Plus} alt="" />
+                <section>
+                    <details>
+                        <summary>O perpétuo pode atrapalhar o resultado do lançamento?</summary>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis 
+                            facere quisquam similique cum laudantium dolorem vitae accusamus iste 
+                            corrupti atque possimus voluptatum placeat, illum, deleniti fuga rerum 
+                            modi est aliquid.
+                        </p>
+                    </details>
+                </section>
             </div>           
         </div>
     )
