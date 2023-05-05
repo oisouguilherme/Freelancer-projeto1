d6ec71f755d5dcbd34ed4b9cf722a2f039540595
Guilherme Henrique <gh.dourado77@gmail.com>
2023-05-05 15:31:03 -0300

    alterando imagens e tirando coisas desnecessarias

--- src/components/SobreCurso/SobreCurso.jsx
+++ src/components/SobreCurso/SobreCurso.jsx
@@ -1,18 +1,16 @@
 import "./SobreCurso.css";
-import ImagemPessoal from "../../assets/ulisses.jpeg";
+import ImagemPessoal from "../../assets/ulisses.png";
 
 function SobreCurso() {
   return (
     <div className="sobreContain">
       <div className="sobre-curso">
-        <h2>
-          De onde veio o <br /> VQC WhatsApp ?
-        </h2>
+        <h2>Com quem vai aprender</h2>
         <p>
-          Se você ainda não vende pelo WhatsApp, em pouco tempo ficará para
-          trás. Isso porque o potencial da ferramenta em gerar vendas é imenso.
-          Hoje, no Brasil são 147 milhões de usuários de todas as idades.
-          Imagine se cada um deles fosse um potencial cliente seu!
+          Sou Ulisses, formado em Engenharia, especialista em administração
+          estratégica, tenho cursos de marketing e vendas pela ESPM, O Novo
+          Mercado, RD Station e Empiricus. Trabalho com comunicação e vendas há
+          8 anos.
         </p>
       </div>
       <div className="img-curso">
